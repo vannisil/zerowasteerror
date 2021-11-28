dbb2f28aedc678b342464d08e82b9335ab4e3ed9
vannisil <g.silvestri3@studenti.poliba.it>
2021-11-28 14:01:23 +0100

    Prima versione front-end

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {MatButtonModule} from '@angular/material/button';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { InizioComponent } from './inizio/inizio.component';
+import { RicicloComponent } from './riciclo/riciclo.component';
+import { FormComponent } from './form/form.component';
+import {MatSelectModule} from '@angular/material/select';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    InizioComponent,
+    RicicloComponent,
+    FormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatSelectModule,
+    RouterModule.forRoot([
+      {path:'inizio', component: InizioComponent},
+      {path: '', redirectTo: '/inizio', pathMatch: 'full'},
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
